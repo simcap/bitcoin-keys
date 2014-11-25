934bbc0e6855fe104777dae308713b7f09435e1d
simcap <simcap@fastmail.com>
2014-11-25 17:07:15 +0100

    Implementing base58

--- lib/bitcoin/keys.rb
+++ lib/bitcoin/keys.rb
@@ -4,6 +4,8 @@ require 'digest'
 
 module Bitcoin
   module Keys
+
+    BASE_58_ALPHABET = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz".freeze
     
     def self.generate
       # Elliptic curve container
@@ -17,17 +19,34 @@ module Bitcoin
       [priv_key_as_hex, pub_key_as_hex]
     end
 
-    def self.convert_to_bitcoin_address(hex_public_key)
-      pub_key_as_string = [hex_public_key].pack('H*')
-      sha = Digest::SHA256.digest(pub_key_as_string)
-      rmd = Digest::RMD160.digest(sha)
-      rmd_hex = rmd.unpack('H*').first
-      extended_rmd_as_hex = '00' + rmd_hex
-      extended_rmd = [extended_rmd_as_hex].pack('H*') 
-      sha = Digest::SHA256.digest(extended_rmd)
+    def self.convert_to_bitcoin_address(pubkey_hex)
+      pubkeyhash = self.pubkey_hash(pubkey_hex)
+      self.base_58_check([pubkeyhash].pack('H*'))
+    end
+
+    def self.pubkey_hash(pub_key_hex, version = '00')
+      bytes = [pub_key_hex].pack('H*')
+      digest = Digest::RMD160.hexdigest(Digest::SHA256.digest(bytes))
+      hex = version + digest
+      raw = [hex].pack('H*')
+      sha = Digest::SHA256.digest(raw)
       sha = Digest::SHA256.digest(sha)
-      first_four_bytes_as_hex = sha.unpack('H8').first
-      result = extended_rmd_as_hex + first_four_bytes_as_hex 
+      (raw + sha[0,4]).unpack('H*').first
+    end
+
+    def self.base_58_check(raw)
+      leading_zeros = raw.bytes.take_while {|c| c == 0}.size
+      bignum = (raw.unpack('H*').first).to_i(16)
+      result = ""
+      while bignum > 0
+        bignum, remainder = bignum.divmod(58)
+        result << BASE_58_ALPHABET[remainder]
+      end
+      result = result.each_char.drop_while {|e| e == BASE_58_ALPHABET[0]}.join
+      leading_zeros.times do
+        result << BASE_58_ALPHABET[0]
+      end
+      result.reverse
     end
 
   end
