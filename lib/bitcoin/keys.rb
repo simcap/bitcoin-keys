8874d0646d2707b2ace8c78df973aae4756de8b5
simcap <simcap@fastmail.com>
2014-11-25 17:07:15 +0100

    WIP base58

--- lib/bitcoin/keys.rb
+++ lib/bitcoin/keys.rb
@@ -4,6 +4,8 @@ require 'digest'
 
 module Bitcoin
   module Keys
+
+    BASE_58_ALPHABET = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz".freeze
     
     def self.generate
       # Elliptic curve container
@@ -28,6 +30,27 @@ module Bitcoin
       sha = Digest::SHA256.digest(sha)
       first_four_bytes_as_hex = sha.unpack('H8').first
       result = extended_rmd_as_hex + first_four_bytes_as_hex 
+      self.base_58_check([result].pack('H*'))
+    end
+
+    def self.base_58_check(raw)
+      raw_hex = raw.unpack('H*').first
+      versionned_raw_hex = '00' + raw_hex
+      versionned_raw = [versionned_raw_hex].pack('H*')
+      sha = Digest::SHA256.digest(versionned_raw)
+      sha = Digest::SHA256.digest(sha)
+      concat = versionned_raw + sha.unpack('a4').first
+      leading_zeros = concat.unpack('H*').first.each_char.take_while {|c| c == '0'}.size
+      bignum = concat.unpack('H*').first.to_i(16)
+      result = ""
+      while bignum > 0
+        bignum, remainder = bignum.divmod(58)
+        result << BASE_58_ALPHABET[remainder]
+      end
+      leading_zeros.times do
+        result << BASE_58_ALPHABET[0]
+      end
+      result.reverse
     end
 
   end
--- test/lib/bitcoin/keys_test.rb
+++ test/lib/bitcoin/keys_test.rb
@@ -4,6 +4,6 @@ class TestBitcoinKeys < MiniTest::Unit::TestCase
   def test_convert_public_key_to_bitcoin_address_as_hex
     pub_key = '0450863AD64A87AE8A2FE83C1AF1A8403CB53F53E486D8511DAD8A04887E5B23522CD470243453A299FA9E77237716103ABC11A1DF38855ED6F2EE187E9C582BA6'
     address = Bitcoin::Keys.convert_to_bitcoin_address(pub_key)
-    assert_equal address, '00010966776006953D5567439E5E39F86A0D273BEED61967F6'.downcase
+    assert_equal address, '16UwLL9Risc3QfPqBUvKofHmBQ7wMtjvM'
   end
 end
