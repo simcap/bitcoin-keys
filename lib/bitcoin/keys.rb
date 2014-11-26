fe5ebbe3c54f623b1b654f63d6472a0d36de4914
simcap <simcap@fastmail.com>
2014-11-26 15:41:26 +0100

    WIP convert to bitcoin address finished

--- lib/bitcoin/keys.rb
+++ lib/bitcoin/keys.rb
@@ -19,40 +19,49 @@ module Bitcoin
       [priv_key_as_hex, pub_key_as_hex]
     end
 
-    def self.convert_to_bitcoin_address(hex_public_key)
-      pub_key_as_string = [hex_public_key].pack('H*')
-      sha = Digest::SHA256.digest(pub_key_as_string)
-      rmd = Digest::RMD160.digest(sha)
-      rmd_hex = rmd.unpack('H*').first
-      extended_rmd_as_hex = '00' + rmd_hex
-      extended_rmd = [extended_rmd_as_hex].pack('H*') 
-      sha = Digest::SHA256.digest(extended_rmd)
-      sha = Digest::SHA256.digest(sha)
-      first_four_bytes_as_hex = sha.unpack('H8').first
-      result = extended_rmd_as_hex + first_four_bytes_as_hex 
-      self.base_58_check([result].pack('H*'))
+    def self.convert_to_bitcoin_address(pubkey_hex)
+      pubkeyhash = self.pubkey_hash(pubkey_hex)
+      #sha = Digest::SHA256.digest(extended_rmd)
+      #sha = Digest::SHA256.digest(sha)
+      #first_four_bytes_as_hex = sha.unpack('H8').first
+      #hex_result = pubkeyhash + first_four_bytes_as_hex 
+      #result = [hex_result].pack('H*')
+      self.base_58_check([pubkeyhash].pack('H*'))
+    end
+
+    def self.pubkey_hash(pub_key_hex, version = '00')
+      bytes = [pub_key_hex].pack('H*')
+      digest = Digest::RMD160.hexdigest(Digest::SHA256.digest(bytes))
+      version + digest
     end
 
     def self.base_58_check(raw)
-      raw_hex = raw.unpack('H*').first
-      versionned_raw_hex = '00' + raw_hex
-      versionned_raw = [versionned_raw_hex].pack('H*')
-      sha = Digest::SHA256.digest(versionned_raw)
+      sha = Digest::SHA256.digest(raw)
       sha = Digest::SHA256.digest(sha)
-      concat = versionned_raw + sha.unpack('a4').first
-      leading_zeros = concat.unpack('H*').first.each_char.take_while {|c| c == '0'}.size
-      bignum = concat.unpack('H*').first.to_i(16)
+      concat = raw + sha[0,4]
+      leading_zeros = concat.bytes.take_while {|c| c == 0}.size
+      bignum = (concat.unpack('H*').first).to_i(16)
       result = ""
       while bignum > 0
         bignum, remainder = bignum.divmod(58)
         result << BASE_58_ALPHABET[remainder]
       end
+      result = result.each_char.drop_while {|e| e == BASE_58_ALPHABET[0]}.join
       leading_zeros.times do
         result << BASE_58_ALPHABET[0]
       end
       result.reverse
     end
 
+
+    def self.bytes_to_integer(hex)
+      raw = [hex].pack('H*')
+      value = 0
+      raw.each_char.each_with_index do |b, i|
+        value = value + ((256 ** i) * b.ord)
+      end
+      value
+    end
   end
 end
 
