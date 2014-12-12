3f4b5c25ce1ca9635b7d7100d6c411699b98006d
simcap <simcap@fastmail.com>
2014-12-12 15:57:49 +0100

    Validating public key; Compressed public keys

--- lib/bitcoin/keys/public_key.rb
+++ lib/bitcoin/keys/public_key.rb
@@ -1,8 +1,21 @@
 module Bitcoin
   module Keys
+
+    class InvalidPublicKeyHex < ArgumentError
+
+      def initialize(reason)
+        @reason = reason 
+      end
+
+      def message
+        "Invalid public key hex. #{@reason}" 
+      end
+    end
+
     class PublicKey
 
       def initialize(pubkey_hex)
+        self.class.validate(pubkey_hex)
         @pubkey_hex = pubkey_hex
       end
 
@@ -20,6 +33,12 @@ module Bitcoin
         @pubkey_hex
       end
 
+      def compressed
+        x, y = coordinates 
+        prefix = y.to_i(16).even? ? '02' : '03'
+        prefix + x
+      end
+
       def address
         @pubkey_address ||= begin
           Base58.encode58([self.to_hash].pack('H*'))
@@ -28,6 +47,27 @@ module Bitcoin
 
       def to_s
         self.address
+      end
+
+      def self.validate(pubkey_hex)
+        unless String === pubkey_hex
+          raise InvalidPublicKeyHex.new('Expecting type String')
+        end
+        unless (invalids = pubkey_hex.downcase.delete('0123456789abcdef')).empty?
+          raise InvalidPublicKeyHex.new("Non hex characters #{invalids}.")
+        end
+        unless pubkey_hex[0..1] == "04"
+          raise InvalidPublicKeyHex.new('Expecting uncompressed key with \'04\' prefix.')
+        end
+        unless pubkey_hex.bytesize == 130
+          raise InvalidPublicKeyHex.new('Wrong key length.')
+        end
+      end
+
+      private 
+
+      def coordinates
+        @coordinates ||= [@pubkey_hex[2..65], @pubkey_hex[66..130]]
       end
 
     end
--- test/lib/bitcoin/keys/public_key_test.rb
+++ test/lib/bitcoin/keys/public_key_test.rb
@@ -8,10 +8,35 @@ class TestPublicKey < MiniTest::Test
     assert_equal pubkey.address, '16UwLL9Risc3QfPqBUvKofHmBQ7wMtjvM'
   end
 
+  def test_pubkey_compressed
+    pubkey_hex = '0479be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8'
+    pubkey = Bitcoin::Keys::PublicKey.new(pubkey_hex)
+    assert_equal pubkey.compressed, '0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798'
+  end
+
   def test_pubkey_hash
     pubkey_hex = '0450863AD64A87AE8A2FE83C1AF1A8403CB53F53E486D8511DAD8A04887E5B23522CD470243453A299FA9E77237716103ABC11A1DF38855ED6F2EE187E9C582BA6'
     pubkey = Bitcoin::Keys::PublicKey.new(pubkey_hex)
     assert_equal pubkey.to_hash, '00010966776006953d5567439e5e39f86a0d273beed61967f6' 
+  end
+
+  def test_validate_hex_pubkey
+    assert_raises Bitcoin::Keys::InvalidPublicKeyHex do |error|
+      Bitcoin::Keys::PublicKey.new(8)
+      assert_match /String/, error.message
+    end
+    assert_raises Bitcoin::Keys::InvalidPublicKeyHex do |error|
+      Bitcoin::Keys::PublicKey.new('04gzAASS')
+      assert_match /gzss/, error.message
+    end
+    assert_raises Bitcoin::Keys::InvalidPublicKeyHex do |error|
+      Bitcoin::Keys::PublicKey.new('05')
+      assert_match /uncompressed.*prefix/, error.message
+    end
+    assert_raises Bitcoin::Keys::InvalidPublicKeyHex do |error|
+      Bitcoin::Keys::PublicKey.new('0434567aa6789f56789e')
+      assert_match /length/, error.message
+    end
   end
 
 end
