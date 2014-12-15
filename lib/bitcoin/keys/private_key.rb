6b018995a0295a04a0b7bc7ce5d325e571576143
simcap <simcap@fastmail.com>
2014-12-15 17:22:12 +0100

    Private key: compressed WIF format

--- lib/bitcoin/keys/private_key.rb
+++ lib/bitcoin/keys/private_key.rb
@@ -2,26 +2,35 @@ module Bitcoin
   module Keys
     class PrivateKey
 
-      def initialize(privkey_hex)
+      def initialize(privkey_hex, network = :mainnet)
         @privkey_hex = privkey_hex
+        @network = case network
+                   when :testnet then 'ef'  
+                   when :mainnet then '80'  
+                   else raise 'Cannot build key for this network' 
+                   end
       end
 
-      def wif(network = '80')
-        @wif ||= begin
-          versionned = network + @privkey_hex 
-          sha = Hasher.hash256_as_hex([versionned].pack('H*'))
-          Base58.encode58([versionned + sha[0,8]].pack('H*'))
-        end
+      def wif
+        @wif ||= Base58.encode58(binary_wif(@privkey_hex))
       end
 
-      def to_wif
-        self.wif
+      def wif_compressed
+        @wif_compressed ||= Base58.encode58(binary_wif(@privkey_hex + '01'))
       end
 
       def to_hex
         @privkey_hex
       end
       alias_method :to_s, :to_hex
+
+      private
+
+      def binary_wif(hex_key)
+        versionned = @network + hex_key
+        sha = Hasher.hash256_as_hex([versionned].pack('H*'))
+        [versionned + sha[0,8]].pack('H*')
+      end
     end
   end
 end
--- test/lib/bitcoin/keys/private_key_test.rb
+++ test/lib/bitcoin/keys/private_key_test.rb
@@ -5,12 +5,19 @@ class TestPrivateKey < MiniTest::Test
   def test_wif_format
     privkey_hex = '1E99423A4ED27608A15A2616A2B0E9E52CED330AC530EDCC32C8FFC6A526AEDD'
     privkey = Bitcoin::Keys::PrivateKey.new(privkey_hex)
-    assert_equal privkey.to_wif, '5J3mBbAH58CpQ3Y5RNJpUKPE62SQ5tfcvU2JpbnkeyhfsYB1Jcn'
     assert_equal privkey.wif, '5J3mBbAH58CpQ3Y5RNJpUKPE62SQ5tfcvU2JpbnkeyhfsYB1Jcn'
+    assert_equal privkey.wif_compressed, 'KxFC1jmwwCoACiCAWZ3eXa96mBM6tb3TYzGmf6YwgdGWZgawvrtJ' 
 
     privkey_hex = '0C28FCA386C7A227600B2FE50B7CAE11EC86D3BF1FBE471BE89827E19D72AA1D'
     privkey = Bitcoin::Keys::PrivateKey.new(privkey_hex)
-    assert_equal privkey.to_wif, '5HueCGU8rMjxEXxiPuD5BDku4MkFqeZyd4dZ1jvhTVqvbTLvyTJ'
     assert_equal privkey.wif, '5HueCGU8rMjxEXxiPuD5BDku4MkFqeZyd4dZ1jvhTVqvbTLvyTJ' 
+
+    assert_equal privkey.wif_compressed, 'KwdMAjGmerYanjeui5SHS7JkmpZvVipYvB2LJGU1ZxJwYvP98617' 
+  end
+
+  def test_throw_when_constructor_has_invalid_network 
+    assert_raises RuntimeError do
+      Bitcoin::Keys::PrivateKey.new('stuff', :rubbish)
+    end
   end
 end
